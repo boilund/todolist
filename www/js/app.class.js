8cd7ef11c2d111f3d91824cd04ecf86155c28c0c
Nana Mochihara <nanahaan@gmail.com>
2017-12-11 12:20:15 +0100

    update layout for good usability

--- www/js/app.class.js
+++ www/js/app.class.js
@@ -9,19 +9,19 @@ class App {
     let i = 0;
     for (let item of this.toDoList.items) {
       $('.to-do-lists').append(`
-      <li class="list-group-item list-group-item-action" index="${i}">
+      <li class="list-group-item list-group-item-action pr-0" index="${i}">
         <div class="form-check">
           <label class="form-check-label">
             <input id="check" class="form-check-input" type="checkbox" value="${item.name}">
               ${item.name}
           </label>
-          <div class="dropdown float-right">
-            <button class="btn btn-primary btn-sm dropdown-toggle pl-1" type="button" id="dropdownMenuButton" data-toggle="dropdown">
-            </button>
-            <div class="dropdown-menu p-3" aria-labelledby="dropdownMenuButton">
-              <p>Priority:  ${item.priority}</p>
-              <button id="remove-this" class="dropdown-item　btn btn-secondary mt-2">
-              Delete
+          <div class="btn-toolbar float-right">
+            <div>
+              <button id="primary" class="btn btn-sm btn-dark rounded-circle">
+              ${item.priority}
+              </button>
+              <button id="remove-this" class="btn btn-link text-dark">
+              <i class="fa fa-times" aria-hidden="true"></i>
               </button>
             </div>
           </div>
